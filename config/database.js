39e39dfb4315308af9c4b785593553f3f0d3f649
Astaprasad Das <astaprasaddas3950@gmail.com>
2021-09-06 17:58:24 +0530

    Update Database string

--- config/database.js
+++ config/database.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 
+const DB = "mongodb+srv://astaprasad:Hargram3950@cluster0.dqabi.mongodb.net/spakassignment?retryWrites=true&w=majority"
 
-try{
-    mongoose.connect(process.env.DB_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-        },
-        (err,result)=>{
-            if(!err){
-                console.log('Database Connected');
-            }
-    });
-}catch(err){
-    console.log('Database not connected')
-}
+    mongoose.connect(DB,{
+        useNewUrlParser:true,
+        useCreateIndex:true,
+        useUnifiedTopology:true,
+        useFindAndModify:false
+
+    }).then(()=>{
+        console.log('Connection Success');
+    }).catch(err=>{
+        console.log('Connections Faild  Error:  '+err);
+    })
